e0862c5ce29dc17aa23f5fdc901ffdac5f798819
van350 <v.chris23@gmail.com>
2017-07-12 22:21:07 -0500

    include additional images for settings and jpanel that includes the setting icon. (NOTE: may want to retract this for first version)

--- src/term/project/cis350/CIS350TermProject.java
+++ src/term/project/cis350/CIS350TermProject.java
@@ -33,21 +33,27 @@ public class CIS350TermProject {
 	static JFrame f;								// holds the main JFrame of the Application
 	static final int F_PADDING = 40 /3;				// defines padding around 'f' JFrame
 	static JPanel p;								// This Panel holds the Arrow ICONS	
-	static JPanel p2;								// This Panel holds the comboBoxes and JButton
-	static final int P2_TOP_PAD = 225;				// Bottom Padding for 'P2' proper placement
 	static JLabel background;						// Object used to hold current movie poster displayed
 	static JLabel rightArrow;						// Holds the right arrow Icon image 
 	static final int RIGHT_ARROW_LEFT_PAD = 0;		// Controls Left Padding for proper Right Arrow icon placement  
+	static JLabel setting;							// Holds the setting menu Icon image
+	static final int SETTING_TOP_PAD = -200;		// Controls the top padding for the setting icon
 	static JLabel leftArrow;						// Holds the left arrow Icon image
+	static final int RL_ARROW_TOP_PAD = 100;		// Controls the top padding for right and left arrow image icons
 	static final double ARROW_PERC_WID = 0.5;		// represents the percentage of panel width the arrow should take
 	static final double ARROW_PERC_HEI = 0.5; 		// represents the percentage of panel height the arrow should take
 	static final int LEFT_ARROW_LEFT_PAD = -175 /3;	// Controls Left Padding for proper Left Arrow icon placement
 	static final int LEFT_ARROW_RIGHT_PAD = 90;		// Controls Right Padding for proper Left Arrow icon placement
 	
+
+	static JPanel p2;								// This Panel holds the comboBoxes and JButton
+	static final int P2_TOP_PAD = 					// Bottom Padding for 'P2' proper placement
+				225 -  RL_ARROW_TOP_PAD;			
+	
 	static JComboBox<String> 	genre;				// Holds genre related information for movie selection
 	static JComboBox<String> 	era;				// Holds era related information for movie selection
 	static JComboBox<String>	othersRating;		// holds rating related information for movie selection
-	static JButton 		getSuggestion;				// button that is used to get a movie suggestion
+	static JButton 				getSuggestion;		// button that is used to get a movie suggestion
 	
 	static int maxStars;							// defines the max star rating for a movie.
 	static int minStars;							// defines the min star rating for a movie.
@@ -76,7 +82,17 @@ public class CIS350TermProject {
 		//Initiating the first movie poster
 	    background = new JLabel( movieList.getMoviePoster(POSTERHEIGHT, POSTERWIDTH) );
 	    //generating the right arrow icon image in the JLabel and creating a mouseListener
-	    // event
+	    //event
+	    ImageIcon settingImgIcon = new ImageIcon(System.getProperty("user.dir") + "/Hamburger_icon.svg2.png");
+	    setting = new JLabel( settingImgIcon );
+	    setting.addMouseListener(new MouseAdapter(){
+	      	  public void mousePressed(MouseEvent e){
+	      		  getClickInfo(e);
+	      	  }
+	    });
+	    
+	    
+	    
 	    ImageIcon rArrow = new ImageIcon(System.getProperty("user.dir") + "/rightHollow2.png");
 	    rightArrow = new JLabel( rArrow );
 	    rightArrow.addMouseListener(new MouseAdapter(){
@@ -153,7 +169,7 @@ public class CIS350TermProject {
 			background = new JLabel( movieList.getMoviePoster(POSTERHEIGHT, POSTERWIDTH) );
 		}else if(e.getSource() == leftArrow){
 			//setting personal rating for currently displayed poster
-			// to the minimal star rating. 
+			// to the minimal star rating.
 			movieList.setMovieScore(minStars);
 			//getting new background poster for rating. 
 			background = new JLabel( movieList.getMoviePoster(POSTERHEIGHT, POSTERWIDTH) );
@@ -163,6 +179,10 @@ public class CIS350TermProject {
 			// current movie on screen. 
 			//getting new background poster for rating. 
 			background = new JLabel( movieList.getMovieToWatch(POSTERHEIGHT, POSTERWIDTH) );
+		}else if(e.getSource() == setting){
+			System.out.println("Hit Setting");
+				
+		
 		}
 
 	}
@@ -209,18 +229,21 @@ public class CIS350TermProject {
       	f.setLayout(new GridBagLayout());
       	
       	// initializing the GridBagConstraints. 
-      	gbc = new GridBagConstraints();      	
-      	//setting the Padding associated with proper placement of the right arrow icon      	
-      	gbc.insets = new Insets(0,RIGHT_ARROW_LEFT_PAD,0,0);		
+      	gbc = new GridBagConstraints();
       	// setting the vertical and horizontal location for the gbc
       	gbc.gridx = P_HORI_POS + 1;
       	gbc.gridy = P_VERT_POS;
+      	//setting the Padding associated with proper placement of the right arrow icon      	
+      	gbc.insets = new Insets(SETTING_TOP_PAD,RIGHT_ARROW_LEFT_PAD,0,0);	
+      	p.add( setting, gbc);
+      	//setting the Padding associated with proper placement of the right arrow icon      	
+      	gbc.insets = new Insets(RL_ARROW_TOP_PAD,RIGHT_ARROW_LEFT_PAD,0,0);		
       	p.add( rightArrow, gbc);
       	// setting the vertical and horizontal location for the gbc
       	gbc.gridx = P_HORI_POS - 1;
       	gbc.gridy = P_VERT_POS;
       	//setting the padding associated with the proper placement of the left arrow icon
-      	gbc.insets = new Insets(0,LEFT_ARROW_LEFT_PAD,0,LEFT_ARROW_RIGHT_PAD);
+      	gbc.insets = new Insets(RL_ARROW_TOP_PAD,LEFT_ARROW_LEFT_PAD,0,LEFT_ARROW_RIGHT_PAD);
       	p.add(leftArrow, gbc);
       	// setting the vertical and horizontal location for the gbc
       	gbc.gridx = FRAME_POS;
